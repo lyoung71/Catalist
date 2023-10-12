47becaa42275e52d9e6ce6f96888d145fb2e9fad
Hamzah <hamzahrahman123@gmail.com>
2023-10-12 15:53:35 -0400

    client.py file

--- api/queries/client.py
+++ api/queries/client.py
@@ -11,18 +11,3 @@ class Queries:
     def collection(self):
         db = client[self.DB_NAME]
         return db[self.COLLECTION]
-
-
-import os
-import pymongo
-
-
-MONGO_URL = os.environ["MONGO_URL"]
-client = pymongo.MongoClient(MONGO_URL)
-
-
-class Queries:
-    @property
-    def collection(self):
-        db = client[self.DB_NAME]
-        return db[self.COLLECTION]
