87dcfcd507c55ebdea00ed111be08c58753669a2
Hamzah <hamzahrahman123@gmail.com>
2023-10-19 17:17:13 -0400

    some css on journalList

--- ghi/src/components/LoginForm.js
+++ ghi/src/components/LoginForm.js
@@ -16,19 +16,23 @@ const LoginForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     login(inputs.username, inputs.password);
-    // navigate("/")
+    navigate("/");
   };
   return (
-    <form>
-      <label>
+    <form className="form-container">
+      <label className="form-label">
         Username:
         <input type="text" name="username" onChange={handleChange} />
       </label>
-      <label>
+      <label className="form-label">
         Password:
         <input type="password" name="password" onChange={handleChange} />
       </label>
-      <button type="button" onClick={handleSubmit}>
+      <button
+        type="button"
+        onClick={handleSubmit}
+        className="bg-PokeBlue text-PokeYellow hover:bg-opacity-80 rounded px-4 py-2"
+      >
         Login
       </button>
     </form>
@@ -36,3 +40,42 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
+// import useToken from "@galvanize-inc/jwtdown-for-react";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const LoginForm = () => {
+//   const [inputs, setInputs] = useState({});
+
+//   const handleChange = (event) => {
+//     const name = event.target.name;
+//     const value = event.target.value;
+//     setInputs((values) => ({ ...values, [name]: value }));
+//   };
+//   const { login } = useToken();
+//   const navigate = useNavigate();
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     login(inputs.username, inputs.password);
+//     navigate("/");
+//   };
+//   return (
+//     <form>
+//       <label>
+//         Username:
+//         <input type="text" name="username" onChange={handleChange} />
+//       </label>
+//       <label>
+//         Password:
+//         <input type="password" name="password" onChange={handleChange} />
+//       </label>
+//       <button type="button" onClick={handleSubmit}>
+//         Login
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default LoginForm;
