232bda979623c196bccd3c96c08e116082aa8f27
Landon <landonyoung71@gmail.com>
2023-10-31 17:09:52 -0300

    debugging

--- ghi/src/components/PokeTest.js
+++ ghi/src/components/PokeTest.js
@@ -8,20 +8,21 @@ export default function Poke() {
         Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
     let pokeDay = dayOfYear(new Date())
 
-    const fetchData = async () => {
-        const url = `https://pokeapi.co/api/v2/pokemon-species/${pokeDay}`
-        const response = await fetch(url)
-        if (response.ok) {
-            const data = await response.json()
-            console.log(data)
-            setPokemon(data.name)
-            setHappiness(data.base_happiness)
-            setFlavortext(data.flavor_text_entries[1]["flavor_text"])
-        }
-    }
+
 
     useEffect(() => {
-        fetchData()
+        const fetchData = async () => {
+            const url = `https://pokeapi.co/api/v2/pokemon-species/${pokeDay}`
+            const response = await fetch(url)
+            if (response.ok) {
+                const data = await response.json()
+                console.log(data)
+                setPokemon(data.name)
+                setHappiness(data.base_happiness)
+                setFlavortext(data.flavor_text_entries[1]["flavor_text"])
+            }
+        };
+        fetchData();
     }, [])
 
     return (
