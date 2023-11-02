daf0e3bd00533b72ad823f41d416ef6f935c27d6
Landon <landonyoung71@gmail.com>
2023-11-02 12:38:40 -0300

    testing

--- ghi/src/components/PokeTest.js
+++ ghi/src/components/PokeTest.js
@@ -1,5 +1,6 @@
 import { React, useEffect, useState } from "react";
 
+
 export default function Poke() {
     const [pokemon, setPokemon] = useState('')
     const [happiness, setHappiness] = useState('')
@@ -21,8 +22,18 @@ export default function Poke() {
     }
 
     useEffect(() => {
-        fetchData()
-    }, [])
+        const fetchData = async () => {
+            const url = `https://pokeapi.co/api/v2/pokemon-species/${pokeDay}`
+            const response = await fetch(url)
+            if (response.ok) {
+                const data = await response.json()
+                console.log(data)
+                setPokemon(data.name)
+                setHappiness(data.base_happiness)
+                setFlavortext(data.flavor_text_entries[1]["flavor_text"])
+            }
+        }; fetchData()
+    }, [pokeDay])
 
     return (
         <>
